911fdc94efbc131b56c7406fcf1384a27f4ca5e7
keiki <c870350172@gmail.com>
2023-03-19 02:03:11 +0900

    fix xml_to_yolo.py and create LABELS/

--- NEU_defect/xml_to_yolo.py
+++ NEU_defect/xml_to_yolo.py
@@ -26,26 +26,26 @@ def conver_annotation(image_name):
     in_file = open('./ANNOTATIONS/' + image_name[:-3] + 'xml')
     out_file = open('./LABELS/' + image_name[:-3] + 'txt','w')
     tree = ET.parse(in_file)
-    root = tree.getroot
+    root = tree.getroot()
     size = root.find('size')
     w = int(size.find('width').text)
     h = int(size.find('height').text)
 
-    for obj in root.iter('obeject'):
+    for obj in root.iter('object'):
         cls = obj.find('name').text
         if cls not in classes:
             print(cls)
             continue
         cls_id = classes.index(cls)
         xmlbox = obj.find('bndbox')
-        b = (float(xmlbox.find('xmin').text), float(xmlbox.fin('xmax').text),float(xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))
+        b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text),float(xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))
         bb = convert((w,h), b)
         out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
 
 wd = getcwd()
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     for image_path in glob.glob("./IMAGES/*.jpg"):
         image_name = image_path.split('\\')[-1]
-        print(image_path)
+        #print(image_path)
         conver_annotation(image_name)
